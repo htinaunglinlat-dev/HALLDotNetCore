73d38d5d56233035c15c690b61f2567b55cbefbe
Htin Aung Linn Latt <htinaunglinlat1303@gmail.com>
2024-11-16 21:01:22 +0630

    ADD: ConsoleApp2 dependency

---
+++ DotNetTrainingBatch5.ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using DotNetTrainingBatch5.Database.Models;
+
+AppDbContext db = new AppDbContext();   
+var lst = db.TblBlogs.ToList();
+
+foreach(var item in lst)
+{
+    Console.WriteLine($"{item.BlogTitle} {item.BlogContent}");
+}
