2df584aa7e7d02bd7c3d0ec3eee8ea55b1f40261
Htin Aung Linn Latt <htinaunglinlat1303@gmail.com>
2024-11-16 21:00:21 +0630

    feat:  database first

---
+++ DotNetTrainingBatch5.Database/Models/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetTrainingBatch5.Database.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblBlog> TblBlogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            string connectionString = "Data Source=DESKTOP-UST9CM1\\SQLEXPRESS;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblBlog>(entity =>
+        {
+            entity.HasKey(e => e.BlogId).HasName("PK_Tbl_Blog");
+
+            entity.ToTable("tbl_blog");
+
+            entity.Property(e => e.BlogAuthor).HasMaxLength(50);
+            entity.Property(e => e.BlogTitle).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
