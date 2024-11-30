c57a26347d21f033d6b75d100f967bdbc2a492ff
Htin Aung Linn Latt <htinaunglinlat1303@gmail.com>
2024-11-30 21:03:56 +0630

    feat: homework HttpClient

---
+++ ConsoleAppHW/HttpClientExample.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleAppHW
+{
+    public class HttpClientExample
+    {
+        private readonly HttpClient _client;
+        private readonly string _postEndPoint = "https://fakestoreapi.com/products";
+        public HttpClientExample()
+        {
+            _client = new HttpClient();
+        }
+        public async Task ReadAsync()
+        {
+            var response = await _client.GetAsync(_postEndPoint);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonStr);
+            }
+        }
+        public async Task EditAsync(int id)
+        {
+            var response = await _client.GetAsync($"{_postEndPoint}/{id}");
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonStr);
+            }
+        }
+        public async Task CreateAsync(string title, float price, string desc, string category, string image, float rate, int count) 
+        {
+            ProductModel requestModel = new ProductModel()
+            {
+                title = title,
+                price = price,
+                description = desc,
+                category = category,
+                image = image,
+                rating = new RatingModel ()
+                {
+                    rate = rate,
+                    count = count
+                }
+            };
+            
+            var jsonRequest = JsonConvert.SerializeObject(requestModel);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, Application.Json);
+            var response = await _client.PostAsync(_postEndPoint, content);
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+        public async Task UpdateAsync(int id, string title, float price, string desc, string category, string image, float rate, int count)
+        {
+            ProductModel requestModel = new ProductModel()
+            {
+                title = title,
+                price = price,
+                description = desc,
+                category = category,
+                image = image,
+                rating = new RatingModel()
+                {
+                    rate = rate,
+                    count = count
+                }
+            };
+
+            var jsonRequest = JsonConvert.SerializeObject(requestModel);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, Application.Json);
+            var response = await _client.PutAsync(_postEndPoint, content);
+            if (response.IsSuccessStatusCode) {
+                Console.WriteLine("Updated Successfully.");
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            } else
+            {
+                Console.WriteLine("Updated Failed.");
+            }
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var response = await _client.DeleteAsync($"{_postEndPoint}/{id}");
+            if(response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Successfully Deleted");
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
+
+public class ProductModel
+{
+    public int id { get; set; }
+    public string title { get; set; }
+    public float price { get; set; }
+    public string description { get; set; }
+    public string category { get; set; }
+    public string image { get; set; }
+    public RatingModel rating { get; set; }
+}
+
+public class RatingModel
+{
+    public float rate { get; set; }
+    public int count { get; set; }
+}
+
---
+++ ConsoleAppHW/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppHW;
+
+Console.WriteLine("Hello, World!");
+
+HttpClientExample httpClientExample = new HttpClientExample();
+//await httpClientExample.ReadAsync();
+//await httpClientExample.EditAsync(1);
+//await httpClientExample.EditAsync(101);
+
+// string title, float price, string desc, string category, string image, float rate, int count
+//await httpClientExample.CreateAsync("hello world", 100, "we are description", "cloth", "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", 4, 10);
+
+//await httpClientExample.UpdateAsync(1, "hello world", 100, "we are description", "cloth", "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", 4, 10);
+await httpClientExample.DeleteAsync(1);
+
+
+
+//{ "id":1,"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{ "rate":3.9,"count":120} }
